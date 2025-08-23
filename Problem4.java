ae13ea9284cf310cc6c1fd2a113f5f4444014b4f
vineeta singh <svineeta2311@gmail.com>
2025-08-24 00:29:06 +0530

    Add files via upload

---
+++ Problem4.java
@@ -0,0 +1,22 @@
+public class Problem4 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
+        int[] count = new int[10];
+
+        for (int i = 1; i <= 9; i++) {
+            for (int num : arr) {
+                if (num % i == 0) {
+                    count[i]++;
+                }
+            }
+        }
+
+        // Print in same format {1: 11, 2: 8, ...}
+        System.out.print("{");
+        for (int i = 1; i <= 9; i++) {
+            System.out.print(i + ": " + count[i]);
+            if (i < 9) System.out.print(", ");
+        }
+        System.out.println("}");
+    }
+}
